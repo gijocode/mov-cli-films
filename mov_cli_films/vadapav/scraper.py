5fd67bb0f52517a06b37d691f804e4237b6de6a8
gijocode <gijocode@gmail.com>
2024-04-09 10:41:00 +0530

    formatted

--- mov_cli_films/vadapav/scraper.py
+++ mov_cli_films/vadapav/scraper.py
@@ -16,13 +16,6 @@ from bs4 import BeautifulSoup
 
 __all__ = ("VadapavScraper",)
 
-class VadapavSerial:
-    def __init__(self, data):
-        self.id: str = data["id"]
-        self.name: str = data["name"]
-        self.dir: bool = data["dir"]
-        self.parent: str = data["parent"]
-        self.mtime: str = data["mtime"]
 
 class VadapavScraper(Scraper):
     def __init__(self, config: Config, http_client: HTTPClient) -> None:
@@ -31,40 +24,58 @@ class VadapavScraper(Scraper):
 
     def search(self, query: str, limit: int = 10) -> Iterable[Metadata]:
         search_url = f"{self.base_url}/s/{query}"
-        doc = self.http_client.get(search_url)
-        soup = BeautifulSoup(doc, 'html.parser')
+        search_doc = self.http_client.get(search_url)
+        search_results_soup = BeautifulSoup(search_doc, "html.parser")
         # In this site, all movies are appended with its release year.
         # So it can be used as an inexpensive way to determine the type of media
-        movie_pattern = r'.*\(\d{4}\)$'
-        results=[]
-        for movie in soup.find_all('a',{"class":"directory-entry"}):
-            if re.match(movie_pattern,movie.string):
+        movie_pattern = r".*\(\d{4}\)$"
+        search_results = []
+        for search_result_item in search_results_soup.find_all(
+            "a", {"class": "directory-entry"}
+        ):
+            item_id = search_result_item.get("href").strip("/")
+            if re.match(movie_pattern, search_result_item.string):
                 item_type = MetadataType.MOVIE
-                item_year = movie.string[-5:-1]
-                item_name = movie.string[:-6]
+                item_year = search_result_item.string[-5:-1]
+                item_name = search_result_item.string[:-6]
             else:
                 item_type = MetadataType.SERIES
-                item_year = "Series" # better than an ugly empty ()
-                item_name = movie.string
-            results.append(Metadata(id=movie.get('href').strip('/'),title=item_name,type=item_type,year=item_year))
-        return results
+                item_year = "Series"  # better than an ugly empty ()
+                item_name = search_result_item.string
+            search_results.append(
+                Metadata(
+                    id=item_id,
+                    title=item_name,
+                    type=item_type,
+                    year=item_year,
+                )
+            )
+        return search_results
 
-    def scrape_episodes(self,metadata: Metadata):
+    def scrape_episodes(self, metadata: Metadata):
         seasons_html = self.http_client.get(f"{self.base_url}/{metadata.id}")
-        soup = BeautifulSoup(seasons_html,'html.parser')
-        seasons_entries = soup.find_all('a',{'class':'directory-entry'})[1:]
+        seasons_soup = BeautifulSoup(seasons_html, "html.parser")
+        season_dirs = [
+            dir
+            for dir in seasons_soup.find_all("a", {"class": "directory-entry"})
+            if "Season" in dir.string
+        ]
         result = {}
-        for i,season in enumerate(seasons_entries):
-            season_html = self.http_client.get(self.base_url+season.get('href'))
-            soup = BeautifulSoup(season_html,'html.parser')
-            episodes_entries = [item for item in soup.find_all('a',{'class':'file-entry'}) if item.string[-4:] != '.srt']
-            result[i+1]=len(episodes_entries)
-
+        for i, season in enumerate(season_dirs):
+            season_html = self.http_client.get(self.base_url + season.get("href"))
+            season_soup = BeautifulSoup(season_html, "html.parser")
+            episodes_entries = [
+                episode
+                for episode in season_soup.find_all("a", {"class": "file-entry"})
+                if episode.string[-4:] != ".srt"
+            ]
+            result[i + 1] = len(episodes_entries)
+        print(result)
         return result
 
-    def extract_resolution(self,filename):
+    def extract_resolution(self, filename):
         # Regular expression to extract resolution information
-        match = re.search(r'(\d+)p|4K', filename)
+        match = re.search(r"(\d+)p|4K", filename)
         if match:
             if match.group(1):  # If a numeric resolution is found (e.g., "720p")
                 return int(match.group(1))
@@ -73,63 +84,96 @@ class VadapavScraper(Scraper):
         else:
             return 0  # Default to 0 if resolution is not found
 
-    def scrape(self, metadata: Metadata, episode: Optional[utils.EpisodeSelector] = None) -> Series | Movie:
+    def scrape(
+        self, metadata: Metadata, episode: Optional[utils.EpisodeSelector] = None
+    ) -> Series | Movie:
+        print("hi")
         if episode is None:
             episode = utils.EpisodeSelector()
 
         if metadata.type == MetadataType.MOVIE:
-            files = self.http_client.get(f"{self.base_url}/{metadata.id}")
-            soup = BeautifulSoup(files,'html.parser')
-            resources = [x for x in soup.find_all('a',{"class":"file-entry"}) if x.string[-4:]!='.srt']
-            subtitles = [x for x in soup.find_all('a',{'class':'file-entry'}) if x.string[-4:]=='.srt']
-            subtitle_url = {"en":self.base_url+(subtitles[0].get('data-href') or subtitles.get('href'))} if subtitles else None
-            resource_url = ""
+            mov_dir_html = self.http_client.get(f"{self.base_url}/{metadata.id}")
+            movie_soup = BeautifulSoup(mov_dir_html, "html.parser")
+            mov_files = [
+                x
+                for x in movie_soup.find_all("a", {"class": "file-entry"})
+                if x.string[-4:] != ".srt"
+            ]
 
-            # Always select greatest resolution when there are multiple files
-            max_resolution = -1  # Starting with a resolution that's lower than any possible resolution
-            for resource in resources:
-                resolution = self.extract_resolution(resource.string)
-                if resolution > max_resolution:
-                    max_resolution = resolution
-                    resource_url = resource.get('data-href') or resource.get('href')
-
-            url = self.base_url+resource_url if resource_url else None
-
-            return Movie(
-                url,
-                title = metadata.title,
-                referrer = self.base_url,
-                year = metadata.year,
-                subtitles = subtitle_url
+            subtitles = [
+                x
+                for x in movie_soup.find_all("a", {"class": "file-entry"})
+                if x.string[-4:] == ".srt"
+            ]
+            subtitle_url = (
+                {
+                    "en": self.base_url
+                    + (subtitles[0].get("data-href") or subtitles.get("href"))
+                }
+                if subtitles
+                else None
             )
 
-        season = int(episode.season)
+            # Always select greatest resolution when there are multiple files
+            # Starting with a resolution that's lower than any possible resolution
+            movie_url, max_resolution = "", -1
+            for mov_file in mov_files:
+                resolution = self.extract_resolution(mov_file.string)
+                if resolution > max_resolution:
+                    max_resolution = resolution
+                    movie_url = mov_file.get("data-href") or mov_file.get("href")
+
+            series_url = self.base_url + movie_url
+
+            return Movie(
+                series_url,
+                title=metadata.title,
+                referrer=self.base_url,
+                year=metadata.year,
+                subtitles=subtitle_url,
+            )
+
+        season_no = int(episode.season)
         episode_no = int(episode.episode)
-        url=None
 
-        season_str = "S"+str(season) if season>9 else "S0"+str(season)
-        season_global_str = "Season "+str(season) if season>9 else "Season 0"+str(season)
-        episode_str = "E"+str(episode_no) if episode_no>9 else "E0"+str(episode_no)
+        season_str = "S" + str(season_no) if season_no > 9 else "S0" + str(season_no)
+        season_dir_name = (
+            "Season " + str(season_no) if season_no > 9 else "Season 0" + str(season_no)
+        )
+        episode_str = (
+            "E" + str(episode_no) if episode_no > 9 else "E0" + str(episode_no)
+        )
 
-        files = self.http_client.get(f"{self.base_url}/{metadata.id}")
-        soup = BeautifulSoup(files,'html.parser')
-        resources = soup.find_all('a',{"class":"directory-entry"})[1:]
+        series_dir_html = self.http_client.get(f"{self.base_url}/{metadata.id}")
+        series_soup = BeautifulSoup(series_dir_html, "html.parser")
+        season_directories = series_soup.find_all("a", {"class": "directory-entry"})[1:]
 
-        for season_dir in resources:
-            if season_dir.string==season_global_str:
-                files = self.http_client.get(self.base_url+season_dir.get('href'))
-                soup = BeautifulSoup(files,'html.parser')
-                resources = [x for x in soup.find_all('a',{"class":"file-entry"}) if x.string[-4:]!='.srt']
+        for season_dir in season_directories:
+            print(season_dir)
+            if season_dir.string == season_dir_name:
+                season_dir_html = self.http_client.get(
+                    self.base_url + season_dir.get("href")
+                )
+                season_soup = BeautifulSoup(season_dir_html, "html.parser")
+                episode_files = [
+                    file_entry
+                    for file_entry in season_soup.find_all("a", {"class": "file-entry"})
+                    if file_entry.string[-4:] != ".srt"
+                ]
                 break
-        for resource in resources:
-            if season_str+episode_str in resource.string:
-                url = self.base_url+(resource.get('data-href') or resource.get('href'))
+
+        for episode_file in episode_files:
+            print(episode_file)
+            if season_str + episode_str in episode_file.string:
+                episode_url = self.base_url + (
+                    episode_file.get("data-href") or episode_file.get("href")
+                )
                 break
 
         return Series(
-            url,
-            title = metadata.title,
-            referrer = self.base_url,
-            episode = episode,
-            subtitles = None
+            episode_url,
+            title=metadata.title,
+            referrer=self.base_url,
+            episode=episode,
+            subtitles=None,
         )
